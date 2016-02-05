98deb0a86da3daa6d9c6112aed06f074b5ef0abb
Yash Murty <yashmurty@gmail.com>
2016-02-05 17:07:32 +0530

    BRF Model and Migrations

---
+++ app/BasicRequisitionForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BasicRequisitionForm extends Model
+{
+    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'brfs';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'doctype', 'title', 'publisher','agency',
+        'isbn', 'volumne', 'price','sectioncatalogue',
+        'numberofcopies', 'laravel_id', 'lac_status','librarian_status',
+        'remarks',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
---
+++ database/migrations/2016_02_05_112634_create_brfs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrfsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('brfs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('doctype');
+            $table->string('title');
+            $table->string('publisher');
+            $table->string('agency')->nullable();
+            $table->string('isbn')->nullable();
+            $table->string('volumne')->nullable();
+            $table->string('price')->nullable();
+            $table->string('sectioncatalogue')->nullable();
+            $table->string('numberofcopies')->nullable();
+            $table->string('laravel_id')->nullable();
+            $table->string('lac_status')->nullable();
+            $table->string('librarian_status')->nullable();
+            $table->string('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('brfs');
+    }
+}
