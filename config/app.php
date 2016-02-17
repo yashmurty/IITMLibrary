993e46e3857e58b60e78c3c77fb1a21cb33595c1
Yash Murty <yashmurty@gmail.com>
2016-02-17 14:28:38 +0530

    Excel Library

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        'Maatwebsite\Excel\ExcelServiceProvider',
+
     ],
 
     /*
@@ -202,6 +204,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Input'     => Illuminate\Support\Facades\Input::class,
+        'Excel' => 'Maatwebsite\Excel\Facades\Excel',
 
     ],
 
