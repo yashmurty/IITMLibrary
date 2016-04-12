03288a4de841e8ed39c2505165c382bf6afce81e
Yash Murty <yashmurty@gmail.com>
2016-04-12 17:23:59 +0530

    Admin User Migration

--- database/migrations/2016_02_11_085003_create_admin_users_table.php
+++ database/migrations/2016_02_11_085003_create_admin_users_table.php
@@ -17,6 +17,8 @@ class CreateAdminUsersTable extends Migration
             $table->increments('id');
             $table->string('laravel_user_id');
             $table->string('iitm_id');
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
