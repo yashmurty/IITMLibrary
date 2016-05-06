3a4f6c9e3d11f584420f2b59ce44ac0ace18fdad
Yash Murty <yashmurty@gmail.com>
2016-05-06 11:34:08 +0530

    Modal Title update

--- resources/views/admin/adminrequeststatusviewbrf.blade.php
+++ resources/views/admin/adminrequeststatusviewbrf.blade.php
@@ -70,7 +70,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        <h4 class="modal-title" id="myModalLabel">Deny Request</h4>
       </div>
       <div class="modal-body">
         <div class="form-group">
