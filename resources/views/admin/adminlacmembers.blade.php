688a0e12facde1b96f445304b243a99aaf94a2f7
Yash Murty <yashmurty@gmail.com>
2016-06-03 15:56:03 +0530

    Admin - LAC Members Edit Button Enable

--- resources/views/admin/adminlacmembers.blade.php
+++ resources/views/admin/adminlacmembers.blade.php
@@ -8,40 +8,40 @@
                 <div class="panel-heading">LAC Members</div>
 
                 <div class="panel-body">
-                    
-                    <table class="table"> 
-                        <caption>LAC Members</caption> 
-                        <thead> 
-                            <tr> 
-                                <th>#</th> 
-                                <th>Image</th> 
-                                <th>Faculty</th> 
-                                <th>Employee ID</th> 
-                                <th>Email</th> 
-                                <th>Department Code</th> 
-                                <th>Edit</th> 
-                            </tr> 
-                        </thead> 
-                        <tbody> 
-                            @if(!($lac_users == null))
-                                @foreach ($lac_users as $key => $lac_user) 
-                                <tr> 
-                                    <th scope="row">{{ $key + 1 }}</th> 
-                                    <td><img src="https://photos.iitm.ac.in/byid.php?id={{ $lac_user->iitm_id }}" style="height:50px;"></td> 
-                                    <td>{{ $lac_user->name }}</td> 
-                                    <td>{{ $lac_user->iitm_id }}</td> 
-                                    <td>{{ $lac_user->lac_email_id }}</td> 
-                                    <td>{{ $lac_user->iitm_dept_code }}</td> 
-                                    
 
-                                    <td><a href="#" class="btn btn-danger" disabled>Edit</a></td> 
-                                </tr> 
+                    <table class="table">
+                        <caption>LAC Members</caption>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Image</th>
+                                <th>Faculty</th>
+                                <th>Employee ID</th>
+                                <th>Email</th>
+                                <th>Department Code</th>
+                                <th>Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(!($lac_users == null))
+                                @foreach ($lac_users as $key => $lac_user)
+                                <tr>
+                                    <th scope="row">{{ $key + 1 }}</th>
+                                    <td><img src="https://photos.iitm.ac.in/byid.php?id={{ $lac_user->iitm_id }}" style="height:50px;"></td>
+                                    <td>{{ $lac_user->name }}</td>
+                                    <td>{{ $lac_user->iitm_id }}</td>
+                                    <td>{{ $lac_user->lac_email_id }}</td>
+                                    <td>{{ $lac_user->iitm_dept_code }}</td>
+
+
+                                    <td><a href="#" class="btn btn-danger">Edit</a></td> 
+                                </tr>
                                 @endforeach
                             @else
                                 No LAC Users found.
                             @endif
-                                
-                        </tbody> 
+
+                        </tbody>
                     </table>
 
                 </div>
