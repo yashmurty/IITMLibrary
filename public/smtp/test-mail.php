9fae31e1df4cf60135e608aef7ab1f05d84aef70
Yash Murty <yashmurty@gmail.com>
2017-03-25 02:22:21 +0530

    SMTP Test Script

---
+++ public/smtp/test-mail.php
@@ -0,0 +1,52 @@
+<?php
+// if(isset($_POST['username']) && isset($_POST['password'])){
+// Report all PHP errors (see changelog)
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+// Report all PHP errors
+error_reporting(-1);
+
+// require_once('PHPMailer/class-phpmailer.php');
+require 'PHPMailer/PHPMailerAutoload.php';
+
+	 $mail = new PHPMailer();
+	 $mail->IsHTML(true);
+	 $mail->IsSMTP();
+	 $mail->SMTPAuth = true; // enable SMTP authentication
+	 $mail->SMTPSecure = "ssl"; // sets the prefix to the serviers
+	 $mail->Host = "smtp2.iitm.ac.in"; // sets GMAIL as the SMTP server
+	 $mail->Port = 465; // set the SMTP port for the GMAIL server
+	 $mail->Username = "libsmtp"; // EMAIL username
+	 $mail->Password = "Chennai36"; // EMAIL password
+	 $mail->From = "msoffice@iitm.ac.in"; // "name@yourdomain.com";
+	 $mail->FromName = "DoMS, IIT Madras";  // set from Name
+	 $mail->Subject ="Referee comments for IIT Madras MBA Program";
+	 $mail->Body = "Dear "."name".",<br><br>
+	        You have been nominated as a referee to give
+	        your assessment of the applicant's ability and
+	        suitability for the MBA program at IIT Madras.<br>
+	        Name of the applicant : <b>"."appname"."</b><br><br> Click the link below
+	         to submit your form.<br>
+	<a href=\"http://"."link"."\">"."link"."</a>
+	        <br>
+	        The form would take less than 5 minutes to complete.<br>
+	        <b>If clicking the link above does not take you to the referee form,
+	        you can copy and paste the link in a web browser to access the form.<br>
+	The deadline for receiving your inputs is 18 March 2016.</b>
+	        <br><br> Thanks in anticipation,<br>Admission office,<br>MBA 2016-18 Admission,<br>Department of Management Studies,<br>IIT Madras.";
+	$email = "yashmurty@gmail.com";
+	$mail->AddAddress($email, ""); // to Address
+	$mail->set('X-Priority', '1'); //Priority 1 = High, 3 = Normal, 5 = low
+	if($mail->Send())
+	{
+	echo " Sent to  <br>".$email;
+	}
+	else
+	{
+	echo " not Sent <br>";
+	}
+
+
+?>
